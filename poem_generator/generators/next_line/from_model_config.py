b56c445a2377472ecb67fbdd7e746ce25dc70101
Michele Boggia <michele.boggia@getjenny.com>
2022-02-22 17:11:29 +0200

    implementing next line generation from manual configuration

---
+++ poem_generator/generators/next_line/from_model_config.py
@@ -0,0 +1,96 @@
+import logging
+
+import torch
+from transformers import (
+    MBartTokenizer,
+    MBartForConditionalGeneration,
+)
+
+from poem_generator.io.candidates import PoemLine, PoemLineList
+from poem_generator.utils import filter_candidates
+from poem_generator.io.config import ModelConfig, GenerationConfig
+
+
+# This file contains the code for generating the next line with the wikisource-fi-mbart.pytorch_model.bin model.
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+LANGUAGE_CODES = {"en": "en_XX"}
+
+
+def get_tokenizer_and_model(model_config: ModelConfig):
+
+    tokenizer = MBartTokenizer.from_pretrained(
+        model_config.base_model,
+        src_lang=LANGUAGE_CODES[model_config.lang],
+        tgt_lang=LANGUAGE_CODES[model_config.lang],
+        additional_special_tokens=model_config.special_tokens if model_config.special_tokens else []
+    )
+
+    logging.info("Loading base model {}".format(model_config.base_model))
+    model = MBartForConditionalGeneration.from_pretrained(model_config.base_model)
+    model.config.decoder_start_token_id = tokenizer.lang_code_to_id[
+        LANGUAGE_CODES[model_config.lang]
+    ]
+
+    if model_config.special_tokens:
+        model.resize_token_embeddings(len(tokenizer))  # is this really necessary here?
+    logging.info("Model vocab size is {}".format(model.config.vocab_size))
+    model.load_state_dict(
+        torch.load(model_config.model_file, map_location=torch.device(DEVICE))
+    )
+    model.to(DEVICE)
+
+    return tokenizer, model
+
+
+def generate(
+    poem_state: PoemLineList, tokenizer, model, generation_config: GenerationConfig
+) -> PoemLineList:
+    """
+    Implementation of next line poem generator using mbart for english language
+    :return:
+    """
+    source = generation_config.src_builder(poem_state)
+    encoded = tokenizer.encode(
+        source,
+        padding="max_length",
+        max_length=generation_config.src_max_length,
+        truncation=generation_config.truncation,
+    )
+    if generation_config.batch_multiply:
+        encoded = torch.tensor([encoded] * generation_config.batch_multiply).to(DEVICE)
+        sample_outputs = model.generate(
+            encoded,
+            do_sample=True,
+            max_length=generation_config.out_max_length,
+            temperature=5.,
+            top_k=5,
+        )
+    else:
+        encoded = torch.tensor(encoded).unsqueeze(0).to(DEVICE)
+
+        sample_outputs = model.generate(
+            encoded,
+            do_sample=generation_config.do_sample,
+            max_length=generation_config.out_max_length,
+            num_beams=generation_config.num_beams,
+            # repetition_penalty=5.0,
+            # temperature=0.8,
+            # no_repeat_ngram_size=2,
+            early_stopping=generation_config.early_stopping,
+            num_return_sequences=generation_config.num_return_sequences,
+            decoder_start_token_id=tokenizer.lang_code_to_id[LANGUAGE_CODES["en"]],
+            # use_cache=False,
+            # num_beam_groups=5,
+            top_k=0,
+        )
+
+    candidates = [
+        tokenizer.decode(sample_output, skip_special_tokens=True)
+        for sample_output in sample_outputs
+    ]
+    logging.info("Generated candidates {}".format(candidates))
+
+    return PoemLineList(
+        [PoemLine(text=candidate) for candidate in filter_candidates(candidates)]
+    )
--- poem_generator/io/config.py
+++ poem_generator/io/config.py
@@ -1,4 +1,54 @@
+from typing import Callable, List, Optional
+from poem_generator.io.candidates import PoemLineList
+
+
+class ModelConfig:
+    def __init__(
+        self,
+        base_model: str,
+        model_file: str,
+        lang: str,
+        special_tokens: Optional[List[str]] = None,
+    ):
+        self.base_model = base_model
+        self.model_file = model_file
+        self.lang = lang
+        self.special_tokens = special_tokens
+
+
+class GenerationConfig:
+    def __init__(
+        self,
+        src_builder: Callable[[PoemLineList], str],
+        src_max_length: Optional[int] = 32,
+        truncation: Optional[bool] = True,
+        out_max_length: Optional[int] = 32,
+        do_sample: Optional[bool] = True,
+        num_beams: Optional[int] = 5,
+        batch_multiply: Optional[int] = None,
+        early_stopping: Optional[bool] = False,
+        num_return_sequences: Optional[int] = 5,
+    ):
+        self.src_builder = src_builder
+        self.src_max_length = src_max_length
+        self.truncation = truncation
+        self.out_max_length = out_max_length
+        self.do_sample = do_sample
+        self.num_beams = num_beams
+        self.batch_multiply = batch_multiply
+        self.early_stopping = early_stopping
+        self.num_return_sequences = num_return_sequences
+
+
 class PoemGeneratorConfiguration:
-    def __init__(self, lang, style):
+    def __init__(
+        self,
+        lang: Optional[str] = None,
+        style: Optional[str] = None,
+        next_line_model_config: Optional[ModelConfig] = None,
+        generation_config: Optional[GenerationConfig] = None,
+    ):
         self.lang = lang
         self.style = style
+        self.next_line_model_config = next_line_model_config
+        self.generation_config = generation_config
