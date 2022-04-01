ff7677453a811ba68885c4e76e23cc9d37433af1
Michele Boggia <michele.boggia@getjenny.com>
2022-04-01 14:20:02 +0300

    add job scripts and configs for training next line generation model with keywords

---
+++ jobs/job.puhti.mbart.en.gut.keywords.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=mbart.en.gut.keywords
+#SBATCH --account=project_2005562
+#SBATCH --partition=gpu
+#SBATCH --time=60:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:v100:4
+#SBATCH --mem=32G
+#SBATCH -o slurm.mbart.en.gut.keywords.%J.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=michele.boggia@helsinki.fi
+
+module purge
+module load gcc/9.1.0
+module load cuda/11.1.0
+srun sh ./run_training_puhti.sh "../train_gen_model.py ../training_config/mbart.en.gut.keywords.ini"
