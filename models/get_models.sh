5600cd940236e0e6e7fbf93b28766ae40518f488
Michele Boggia <michele.boggia@getjenny.com>
2022-04-01 16:13:49 +0300

    migrate model to model hub

--- models/get_models.sh
+++ models/get_models.sh
@@ -7,6 +7,17 @@
 ##################
 
 # First line
+if [ ! -d poetry-generation-firstline-mbart-ws-fi-sorted ]; then
+    #git clone https://huggingface.co/bmichele/poetry-generation-firstline-mbart-ws-fi-sorted
+    mkdir poetry-generation-firstline-mbart-ws-fi-sorted
+    (
+    cd poetry-generation-firstline-mbart-ws-fi-sorted || exit
+    wget https://huggingface.co/bmichele/poetry-generation-firstline-mbart-ws-fi-sorted/resolve/main/pytorch_model.bin
+    cd ..
+    )
+fi
+
+# Next line
 if [ ! -d poetry-generation-nextline-mbart-ws-fi-single ]; then
     #git clone https://huggingface.co/bmichele/poetry-generation-nextline-mbart-ws-fi-single
     mkdir poetry-generation-nextline-mbart-ws-fi-single
